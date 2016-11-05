1dcea29b2e515d29c2e57eea1b2e37e09880d322
illynabruin92 <alvaivette@g.ucla.edu>
2016-11-04 21:29:03 -0700

    created mongoose connection

--- server.js
+++ server.js
@@ -14,3 +14,20 @@ var request = require('request');
 app.use(bodyParser.urlencoded({
   extended: false
 }));
+
+// make public a static dir
+app.use(express.static('public'));
+
+// Database configuration with mongoose
+mongoose.connect('mongodb://heroku_ndk7h3d0:qkeo5t1qpmkbqs3guk1l6a9eo5@ds057476.mlab.com:57476/heroku_ndk7h3d0');
+var db = mongoose.connection;
+
+// show any mongoose errors
+db.on('error', function(err) {
+  console.log('Mongoose Error: ', err);
+});
+
+// once logged in to the db through mongoose, log a success message
+db.once('open', function() {
+  console.log('Mongoose connection successful.');
+});
